5a6b92d90db10b8b80c29e4e965d69640357c300
Haofei Yin <hf_yin@qq.com>
2025-06-06 12:20:06 +0000

    upd: 更新测试用例

--- test/test_llm.py
+++ test/test_llm.py
@@ -15,16 +15,20 @@ if __name__ == "__main__":
     with Context(task_id=str(uuid.uuid4().hex)) as ctx:
         start = time.time()
         # 使用 LLM 类
-        # llm = LLM("DeepSeek-R1", 'SDU')
-        llm = LLM("doubao-1-5-thinking-pro-250415", "ARK")
-        # llm = LLM("Qwen3-32B", "VLLM")
+        llm = LLM("volcengine/doubao-1-5-lite-32k-250115")
+        # llm = LLM("volcengine/doubao-1-5-thinking-pro-250415")
+        # llm = LLM("doubao-1-5-thinking-pro-250415", "ARK")
+        llm_s = LLM("Qwen3-32B", "VLLM")
 
         prompt = "Write a Python function to calculate the factorial of a number."
-        response1 = llm(prompt, CodeAnswer.model_json_schema()).result()
+        response1 = llm_s(prompt, CodeAnswer.model_json_schema())
+        response2 = llm(prompt)
+
+        response1 = response1.result()
+        response2 = response2.result()
         print(f"Prompt: {prompt}\ntype: {type(response1)}\nResponse:\n{response1}")
         print('=================================')
+        print(f"Prompt: {prompt}\ntype: {type(response2)}\nResponse:\n{response2}")
 
-        response1 = llm(prompt).result()
-        print(f"Prompt: {prompt}\ntype: {type(response1)}\nResponse:\n{response1}")
         end = time.time()
         print(f"Total time taken: {end - start:.2f} seconds")
